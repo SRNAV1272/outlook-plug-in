c52485bff1a02edd92500ed1ebff587570725815
K Sai Rajesh <sairajesh.korla@navajna.com>
2025-12-13 19:48:17 +0530

    simple test

--- src/index.js
+++ src/index.js
@@ -3,5 +3,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+const render = () => {
+  const container = document.getElementById("root");
+  const root = ReactDOM.createRoot(container);
+  root.render(<App />);
+};
+
+// 🔹 If running inside Outlook
+if (typeof Office !== "undefined") {
+  Office.onReady(() => {
+    render();
+  });
+} else {
+  // 🔹 Normal browser (localhost, preview, dev)
+  render();
+}
