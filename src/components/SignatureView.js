cb4f54a6fb9aba0c3bda8600f41782df5146f7a5
K Sai Rajesh <sairajesh.korla@navajna.com>
2025-12-18 08:46:03 +0530

    update

--- src/components/SignatureView.js
+++ src/components/SignatureView.js
@@ -575,9 +575,9 @@ export default function SignatureView({ user, showPreview, apply, showSocialMedi
         // document.body.removeChild(link);
     };
 
-    const bgField = allFields.find(f => f.key === "backgroundImage");
-    const bgImageURL = bgField?.link || bgField?.value || "";
-    const [backgroundImage] = useImage(bgImageURL, "anonymous");
+    // const bgField = allFields.find(f => f.key === "backgroundImage");
+    // const bgImageURL = bgField?.link || bgField?.value || "";
+    const [backgroundImage] = useImage(form?.emailSignatureUrl);
 
     // Compute scaled size (preserve aspect ratio)
     const getScaledSize = () => {
@@ -598,7 +598,7 @@ export default function SignatureView({ user, showPreview, apply, showSocialMedi
         return { w, h, x, y };
     };
 
-    const { w, h, x, y } = getScaledSize();
+    // const { w, h, x, y } = getScaledSize();
 
     const applyHTML = async () => {
         if (!stageRef.current) return;
@@ -819,8 +819,8 @@ export default function SignatureView({ user, showPreview, apply, showSocialMedi
                                                 animation="wave"
                                             />
                                         </Box> */}
-                                        <img src={DefaultTemplate} alt="new" width={'100%'} />
-                                        <Box mt={4} textAlign="center">
+                                        <Box mt={4} textAlign="center" border={'1px solid red'}>
+                                            <img src={DefaultTemplate} alt="new" width={'100%'} />
                                             <Typography fontFamily={"Plus Jakarta Sans"} variant="h6" gutterBottom>
                                                 No Signature Available !
                                             </Typography>
@@ -864,140 +864,14 @@ export default function SignatureView({ user, showPreview, apply, showSocialMedi
                                                     scale={scale}
                                                 >
                                                     <Layer>
-                                                        {backgroundImage && (
-                                                            <KonvaImage
-                                                                image={backgroundImage}
-                                                                x={x}
-                                                                y={y}
-                                                                width={w}
-                                                                height={h}
-                                                                listening={false}
-                                                            />
-                                                        )
-                                                        }
-                                                        {/* ✅ SHAPES (BOTTOM LAYER) */}
-                                                        {allFields?.filter(item =>
-                                                            item?.type === "shape" &&
-                                                            item?.key !== "signatureName" &&
-                                                            item?.key !== "banner" &&
-                                                            item?.key !== "disclaimer" &&
-                                                            item?.key !== "backgroundColor" &&
-                                                            item?.key !== "backgroundImage"
-                                                        ).map((field) => {
-
-                                                            switch (field.shapeType) {
-
-                                                                case "line":
-                                                                    return (
-                                                                        <Line
-                                                                            key={field.key}
-                                                                            points={field.points || []}
-                                                                            stroke={field.stroke || "#000"}
-                                                                            strokeWidth={field.strokeWidth || 2}
-                                                                            x={field.position?.x || 0}
-                                                                            y={field.position?.y || 0}
-                                                                            listening={false}
-                                                                        />
-                                                                    );
-
-                                                                case "rect":
-                                                                    return (
-                                                                        <Rect
-                                                                            key={field.key}
-                                                                            x={field.position?.x || 0}
-                                                                            y={field.position?.y || 0}
-                                                                            width={field.width || 100}
-                                                                            height={field.height || 50}
-                                                                            fill={field.fill || "#000"}
-                                                                            // stroke={field.stroke || "transparent"}
-                                                                            // strokeWidth={field.strokeWidth || 1}
-                                                                            // cornerRadius={field.radius || 0}
-                                                                            listening={false}
-                                                                        />
-                                                                    );
-
-                                                                case "circle":
-                                                                    return (
-                                                                        <Circle
-                                                                            key={field.key}
-                                                                            x={field.position?.x || 0}
-                                                                            y={field.position?.y || 0}
-                                                                            radius={field.radius || 20}
-                                                                            fill={field.fill || "#000"}
-                                                                            // stroke={field.stroke || "transparent"}
-                                                                            // strokeWidth={field.strokeWidth || 1}
-                                                                            listening={false}
-                                                                        />
-                                                                    );
-
-                                                                default:
-                                                                    return null;
-                                                            }
-                                                        })}
-
-
-                                                        {/* ✅ IMAGES (MIDDLE LAYER) */}
-                                                        {allFields?.filter(item =>
-                                                            ["profilePhoto", "logo", "qrCode"].includes(item.key) &&
-                                                            item?.value &&
-                                                            item?.show
-                                                        ).map((field) => (
-                                                            <>
-                                                                <ImagesUsedPreview
-                                                                    key={field.key}
-                                                                    id={field.key}
-                                                                    data={field}
-                                                                    draggable={false}
-                                                                    handleImageClick={handleImageClick}
-                                                                    DefaultQrCodeLogo={DefaultQrCodeLogo}
-                                                                    noQRCodeLogo={noQRCodeLogo}
-                                                                    shortLink={shortLink}
-                                                                />
-                                                            </>
-                                                        ))}
-
-                                                        {/* ✅ TEXT (TOP LAYER) */}
-                                                        {allFields?.filter(item =>
-                                                            item?.type !== "shape" &&
-                                                            !["profilePhoto", "logo", "qrCode"].includes(item.key) &&
-                                                            item?.key !== "signatureName" &&
-                                                            item?.key !== "banner" &&
-                                                            item?.key !== "disclaimer" &&
-                                                            item?.key !== "backgroundColor" &&
-                                                            item?.key !== "backgroundImage"
-                                                        ).map((field) => {
-
-                                                            const parentKeys = Object.keys(options);
-                                                            const childKeys = Object.values(options).flat();
-                                                            const isChildOnly = !parentKeys.includes(field.key) && childKeys.includes(field.key);
-
-                                                            if (!field.show || isChildOnly) return null;
-                                                            return (
-                                                                <PreviewText
-                                                                    allFields={allFields}
-                                                                    field={field}
-                                                                    getOptionTextForKonva={getOptionTextForKonva}
-                                                                    options={options}
-                                                                    card={card}
-                                                                />
-                                                            );
-                                                        })}
-                                                        {/* 🏞 Banner always fixed at bottom, aspect ratio conserved */}
-                                                        {/* // Disclaimer */}
-                                                        {!form?.elements?.find(f => f.key === "disclaimer")?.value && (
-                                                            <Text
-                                                                x={0}
-                                                                y={baseHeight + bannerHeight} // below banner
-                                                                text={form?.elements?.find(f => f.key === "disclaimer")?.value}
-                                                                fontSize={12} // adjust font size if needed
-                                                                fontFamily="Plus Jakarta Sans"
-                                                                fontStyle="normal"
-                                                                fill="#555555"
-                                                                width={baseWidth}
-                                                                align="center"
-                                                                listening={false}
-                                                            />
-                                                        )}
+                                                        <KonvaImage
+                                                            image={backgroundImage}
+                                                            x={0}
+                                                            y={0}
+                                                            width={stageSize.width / (stageSize.width / baseWidth)}
+                                                            height={stageSize.height / (stageSize.height / baseHeight)}
+                                                            listening={false}
+                                                        />
                                                     </Layer>
                                                 </Stage>
                                             </Box>
