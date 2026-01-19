a3247a0e436536794b09ba9aa292fe473d465758
K Sai Rajesh <sairajesh.korla@navajna.com>
2026-01-19 20:55:06 +0530

    setAsync

--- src/App.js
+++ src/App.js
@@ -170,7 +170,7 @@ export default function App({ user }) {
            ✏️ INSERT SIGNATURE
            ========================================= */
 
-        item.body.setSelectedDataAsync(
+        item.body.setAsync(
           `
         <br/><br/>
         <!-- CARD_BYTE_SIGNATURE_START -->
