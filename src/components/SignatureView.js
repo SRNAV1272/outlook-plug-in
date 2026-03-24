2537b783b72f21a2055c6e438e807fd439543202
K Sai Rajesh <sairajesh.korla@navajna.com>
2026-03-24 16:49:54 +0530

    x-addin-secret removed

--- src/components/SignatureView.js
+++ src/components/SignatureView.js
@@ -137,24 +137,12 @@ export default function SignatureView({
     /* ── API fetch ───────────────────────────────────────────── */
     async function renderSignatureOnServer(userEmail) {
         try {
-            const encryptedMail = await encryptEmail(userEmail);
-            // const primaryRes = await fetch(
-            //     "https://enterprise.cardbyte.ai/email-signature/html/outlook/get-active",
-            //     {
-            //         method: "GET",
-            //         headers: {
-            //             'X-Addin-Secret': 'ab8a57b1ec63ba43a1a420da0ccc5dae990bf7fedc56b1ff35543e45023e416a',
-            //             'Content-Type': 'application/json',
-            //             username: encryptedMail
-            //         }
-            //     }
-            // );
+            const encryptedMail = await encryptEmail("operations@irishightech.com");
             const primaryRes = await fetch(
                 "https://newqa-enterprise.cardbyte.ai/email-signature/html/outlook/get-active",
                 {
                     method: "GET",
                     headers: {
-                        'X-Addin-Secret': 'ab8a57b1ec63ba43a1a420da0ccc5dae990bf7fedc56b1ff35543e45023e416a',
                         'Content-Type': 'application/json',
                         username: encryptedMail
                     }
