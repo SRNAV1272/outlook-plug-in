fd734ce64704927e3a3fcf7987e08d402052353c
K Sai Rajesh <sairajesh.korla@navajna.com>
2026-05-29 01:02:25 +0530

    2 sec

--- build/runtime/event-handler-classic.0.0.234.js
+++ build/runtime/event-handler-classic.0.0.234.js
@@ -6719,7 +6719,7 @@ var CONFIG = {
     WRAP_BOTTOM_PX: 40,
 
     // Send handler must complete within Outlook's hard send budget (~5s).
-    SEND_HANDLER_TIMEOUT_MS: 4000,
+    SEND_HANDLER_TIMEOUT_MS: 3000,
 
     // Compose / from-changed handlers have a softer budget.
     COMPOSE_HANDLER_TIMEOUT_MS: 10000,
