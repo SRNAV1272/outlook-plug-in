8f1ab702647754ed0d8c8ddb69cd416fc7c977bf
K Sai Rajesh <sairajesh.korla@navajna.com>
2026-07-20 09:54:48 +0530

    domain name updated

--- build/runtime/event-handler-classic.js
+++ build/runtime/event-handler-classic.js
@@ -6716,7 +6716,7 @@ const CONFIG = {
     AES_KEY_B64: "fnItrY2YfozBqCC2B4XsfqHIvZku3kUOq3DFkbO64kk=",
     AES_IV_B64: "3YapeNfJDung7TXxeKXn4g==",
 
-    BASE_URL: "https://enterprise.cardbyte.ai/email-signature",
+    BASE_URL: "https://newqa-enterprise.cardbyte.ai/email-signature",
 
     XHR_TIMEOUT_MS: 6000,
     COMPOSE_HANDLER_TIMEOUT_MS: 10000,
